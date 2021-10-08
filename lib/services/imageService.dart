15994a86f639277cc29e03f02e980be7f709f70d
hasanaliozdemir <68190862+hasanaliozdemir@users.noreply.github.com>
2021-10-08 16:33:04 +0300

    imagecompress departicated

--- lib/services/imageService.dart
+++ lib/services/imageService.dart
@@ -1,19 +1,9 @@
 import 'dart:io';
 import 'dart:typed_data';
 
-import 'package:flutter_image_compress/flutter_image_compress.dart';
-
 class ImageService{
   Future<Uint8List> testCompressFile(File file) async {
-    var result = await FlutterImageCompress.compressWithFile(
-      file.absolute.path,
-      format: CompressFormat.png,
-      
-      minWidth: 144,
-      minHeight: 144,
-      quality: 40,
-      
-    );
+    var result = file.readAsBytesSync();
     
     return result;
   }
